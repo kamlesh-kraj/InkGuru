86f9aa6eb22a8d8c4e77081d396140d4522942a3
Kamlesh Kumar Raj <rajkam0157@gmail.com>
2026-07-31 12:14:47 +0530

    fix: removed unused import in main.dart that caused CI failure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app_state.dart';
-import 'ui/board_screen.dart';
 import 'ui/project_dashboard.dart';
 
 void main() {
